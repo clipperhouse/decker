d6e86467554d59d099be17a9909c8e3c0cedc453
Matt Sherman <mwsherman@gmail.com>
2016-03-05 16:34:50 -0500

    Formatting

--- decker.js
+++ decker.js
@@ -1,31 +1,31 @@
-(function() {
-	var expand = function(urlattr){
-		var els = document.querySelectorAll('a[href*="//t.co"][' + urlattr + ']');
+(function () {
+    var expand = function (urlattr) {
+        var els = document.querySelectorAll('a[href*="//t.co"][' + urlattr + ']');
 
-		for (var i = 0; i < els.length; i++){
-			var el = els[i];
-			var href = el.getAttribute(urlattr);
-			el.setAttribute('href', href);
-			el.innerHTML = abbrev(href);
-			el.removeAttribute(urlattr);
-			el.removeAttribute('title');
-		}
-	};
+        for (var i = 0; i < els.length; i++) {
+            var el = els[i];
+            var href = el.getAttribute(urlattr);
+            el.setAttribute('href', href);
+            el.innerHTML = abbrev(href);
+            el.removeAttribute(urlattr);
+            el.removeAttribute('title');
+        }
+    };
 
-	var abbrev = function(url){
-		var result = url.replace(/^http(s?)\:\/\//, '');
-		if (result.length > 32) {
-			result = result.substring(0, 32) + "…";
-		}
-		return result;
-	};
+    var abbrev = function (url) {
+        var result = url.replace(/^http(s?)\:\/\//, '');
+        if (result.length > 32) {
+            result = result.substring(0, 32) + "…";
+        }
+        return result;
+    };
 
-	var tco = function(){
-		expand('data-full-url');
-		expand('data-expanded-url');
-	};
+    var tco = function () {
+        expand('data-full-url');
+        expand('data-expanded-url');
+    };
 
-	setInterval(tco, 2000);
+    setInterval(tco, 2000);
     
     // remove side columns
     // already hidden, see styles.css
@@ -33,8 +33,8 @@
     
     var parent = document.getElementById('page-container');
     var children = document.querySelectorAll('.dashboard');
-    
-    for (var i = 0; i < children.length; i++){
-            parent.removeChild(children[i]);
+
+    for (var i = 0; i < children.length; i++) {
+        parent.removeChild(children[i]);
     }
 })();
